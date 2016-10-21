6aa42521ec54f338bb8f785ac9564260faeece00
CoLcY <colcie007@hotmail.com>
2016-10-21 13:30:20 +0800

    init

--- CYCategory/UIKit/UIKit+CoLcY.h
+++ CYCategory/UIKit/UIKit+CoLcY.h
@@ -6,14 +6,3 @@
 //  Copyright © 2016年 CoLcY. All rights reserved.
 //
 
-#import "UIAlertView+CoLcY.h"
-#import "UIButton+CoLcY.h"
-#import "UIColor+CoLcY.h"
-#import "UIControl+CoLcY.h"
-#import "UIImage+CoLcY.h"
-#import "UIImageView+CoLcY.h"
-#import "UILabel+CoLcY.h"
-#import "UIScrollView+CoLcY.h"
-#import "UIView+CoLcY.h"
-#import "UIWebView+CoLcY.h"
-#import "UIGestureRecognizer+CoLcY.h"
